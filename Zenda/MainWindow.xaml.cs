77e2c61e1eca6c2b8c434004ca8fe4876b024f5b
Jesse <jespower2007@gmail.com>
2023-10-31 21:30:03 +0100

    Started working on the GUI

--- Zenda/MainWindow.xaml.cs
+++ Zenda/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿// Main interaction logic for the WPF screen
+// Program written by Jesse and BuilderDemo
+// 
+// Powered by DSCript by Fireboyd78 and WPFDarkTheme by AngryCarrot789
 
 using System;
 using System.Collections.Generic;
@@ -17,14 +20,41 @@ using System.Windows.Shapes;
 
 namespace Zenda
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuFileExitClicked(object sender, RoutedEventArgs e)
+        {
+            var Result = MessageBox.Show(
+                "Are you sure you want to exit?\n Any unsaved changes will be lost!", // Text to display in the MessageBox
+                "Exit Zenda",                                                          // Title of the MessageBox
+                MessageBoxButton.YesNo,                                                // What type of choices the user has
+                MessageBoxImage.Warning);                                              // Pictogram icon to show
+
+            if (Result == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+            else if (Result == MessageBoxResult.No)
+            {
+                // Do nothing because the user regretted his grave mistake of exiting Zenda. I won't forgive that next time
+            }
+        }
+
+        private void MenuHelpCreditsClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Zenda, the universal Driver editor \n" +
+                "Made by Jesse and BuilderDemo7 \n \n" +
+                "Special thanks to: \n" +
+                "Fireboyd78, for DSCript: https://github.com/Fireboyd78/driver-tools/ (no set license) \n" +
+                "AngryCarrot789, for WPFDarkTheme: https://github.com/AngryCarrot789/WPFDarkTheme (MIT License) \n" +
+                "\n" +
+                "            Source: https://github.com/Jesse2431/Zenda", "Credits", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
